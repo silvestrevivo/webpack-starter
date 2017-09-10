848dcb594cffb0be11bf76db8d3e6be3c3434ab8
silvestrevivo <silvestrevivomillan@gmail.com>
2017-09-10 21:22:02 +0200

    new file structure and option of production

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //here goes all you want import to the index.html through index.js
 import '../sass/style.sass'
 
-var say_Hi = () => {
+const say_Hi = () => {
   console.log(`This is an starter template based on webpack
   with sass-loader, babel-loader and automatic browser refresh`);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ var path = require('path');
 //module path require to load the local sever with node
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src') + '/app/js/index.js',
+    entry: path.resolve(__dirname, 'src') + '/js/index.js',
     //entry point to compile and create bundle
+
     output: {
-        path: path.resolve(__dirname, 'dist') + '/app',
+        path: path.resolve(__dirname, 'dist'),
+        //path to output
         filename: 'bundle.js',
-        publicPath: '/app/'
         //this is the result, bundle.js
+        publicPath: ''
+        //public folder
     },
     module: {
         loaders: [
@@ -29,5 +32,16 @@ module.exports = {
                 //css/sass loader to import all type style files
             }
         ]
+    },
+    devServer: {
+      //here comes the configuration of the server
+      contentBase: path.join(__dirname, "dist"),
+      //from this url is read the site
+      compress: true,
+      //type of compression
+      port: 1234,
+      //port to see on the browser
+      stats: 'errors-only'
+      //outprint in the terminal just the errors
     }
 };
