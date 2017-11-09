e2461f7b89d7f3846592390c25e468912447fd89
silvestrevivo <silvestrevivomillan@gmail.com>
2017-11-10 00:26:10 +0100

    postcss with four plugins and browserslist in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
             },
             {
                 test: /\.sass$/,
-                loader: 'style-loader!css-loader!sass-loader'
+                loader: 'style-loader!css-loader!postcss-loader!sass-loader'
             }
         ]
     },
