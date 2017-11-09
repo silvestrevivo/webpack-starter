0600eb37c0d274cddb51739d68517c09c7381160
silvestrevivo <silvestrevivomillan@gmail.com>
2017-11-10 00:09:00 +0100

    watch added to webpack, airbnb style for index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
-//module path require to define entry and out point, as well as public folder
+// Module path require to define entry and out point, as well as public folder
 
 module.exports = {
-
+  // Entry point to compile and create bundle
   entry: path.resolve(__dirname, 'src') + '/js/index.js',
-  //entry point to compile and create bundle
 
+  // Outpoint to compile in bundle.js all the files
   output: {
       path: path.resolve(__dirname, 'dist'),
-      //path to output
+      // Path to output
       filename: 'bundle.js',
-      //this is the result, bundle.js
+      // This is the result, bundle.js
       publicPath: '/dist'
-      //public folder
+      // Public folder
   },
 
   module: {
@@ -26,7 +26,6 @@ module.exports = {
             {
                 test: /\.sass$/,
                 loader: 'style-loader!css-loader!sass-loader'
-                //css/sass loader to import all type style files
             }
         ]
     },
