b48186c67439453e1f7bf4f5353e9eeef988c93d
silvestrevivo <silvestrevivomillan@gmail.com>
2017-11-10 15:21:37 +0100

    jquery added

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,20 @@
 //here goes all you want import to the index.html through index.js
 import '../sass/style.sass';
+import $ from 'jquery';
 
-let parameters = {
-  first: 'template',
-  second: 'webpack'
-};
+$(document).ready(() => {
+  let parameters = {
+    first: 'template',
+    second: 'webpack'
+  };
 
-const {first, second} = parameters;
+  const {first, second} = parameters;
 
-const say_Hi = () => {
-  console.log(`This is an starter ${first} based on ${second}
-  with sass-loader, babel-loader and automatic browser refresh.`);
-}
+  const say_Hi = () => {
+    console.log(`This is an starter ${first} based on ${second}
+    with sass-loader, babel-loader and automatic browser refresh.`);
+  }
 
-say_Hi();
+  say_Hi();
+
+});
