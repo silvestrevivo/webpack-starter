ecccbda02360a48bd9d486bdcfeaf512eadf7d01
silvestrevivo <silvestrevivomillan@gmail.com>
2017-11-10 09:20:05 +0100

    extract text plugin added to bundle css in a separated file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 // Module path require to define entry and out point, as well as public folder
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// Plugin to create a bundle css apart of javascript
 
 module.exports = {
   // Entry point to compile and create bundle
@@ -16,17 +18,35 @@ module.exports = {
   },
 
   module: {
-        loaders: [
+        rules: [
+            {
+                test: /\.css$/,
+                //loader: 'style-loader!css-loader!postcss-loader',
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: ['css-loader', 'postcss-loader']
+                  //loader: 'css-loader!postcss-loader'
+                })
+            },
+            {
+                test: /\.sass$/,
+                //loader: 'style-loader!css-loader!postcss-loader!sass-loader'
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: ['css-loader', 'postcss-loader', 'sass-loader']
+                  //loader: 'css-loader!postcss-loader!sass-loader'
+                })
+            },
             {
                 test: /\.js$/,
                 include: path.resolve(__dirname, 'src'),
                 exclude: /(node_modules)/,
                 loader: 'babel-loader'
-            },
-            {
-                test: /\.sass$/,
-                loader: 'style-loader!css-loader!postcss-loader!sass-loader'
             }
         ]
     },
-}
+    plugins: [
+      new ExtractTextPlugin('style.css')
+      // Name of the css bundle compiled in the './dist/ folder
+    ]
+};
