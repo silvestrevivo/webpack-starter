08b748e305526414381fdaab5ab8cc25a5e83972
silvestrevivo <silvestrevivomillan@gmail.com>
2017-11-23 23:51:42 +0100

    eslint and prettier added

--- webpack.config.js
+++ webpack.config.js
@@ -8,72 +8,72 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 // Sincrinizing devices to check the site
 
 module.exports = {
-  // Entry point to compile and create bundle
-  entry: path.resolve(__dirname, 'src') + '/js/index.js',
+    // Entry point to compile and create bundle
+    entry: `${path.resolve(__dirname, 'src')}/js/index.js`,
 
-  // Outpoint to compile in bundle.js all the files
-  output: {
-      path: path.join(__dirname, 'dist'),
-      // Path to output
-      filename: 'bundle.js',
-      // This is the result, bundle.js
-      publicPath: '/'
-      // Where the files are available in the server
-  },
+    // Outpoint to compile in bundle.js all the files
+    output: {
+        path: path.join(__dirname, 'dist'),
+        // Path to output
+        filename: 'bundle.js',
+        // This is the result, bundle.js
+        publicPath: '/',
+        // Where the files are available in the server
+    },
 
-  devtool: 'inline-source-map',
-  // Tool to map Sass and javascript
+    devtool: 'inline-source-map',
+    // Tool to map Sass and javascript
 
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    stats: 'errors-only'
-  },
-  // Webpack Server
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        stats: 'errors-only',
+    },
+    // Webpack Server
 
-  module: {
+    module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
-                  fallback: 'style-loader',
-                  use: ['css-loader', 'postcss-loader']
-                })
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader'],
+                }),
             },
             {
                 test: /\.sass$/,
                 use: ExtractTextPlugin.extract({
-                  fallback: 'style-loader',
-                  use: [
-                    {loader: 'css-loader', options: {sourceMap: true}},
-                    {loader: 'postcss-loader', options: {sourceMap: true}},
-                    {loader: 'sass-loader', options: {sourceMap: true}}
-                  ]
-                })
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { sourceMap: true } },
+                        { loader: 'postcss-loader', options: { sourceMap: true } },
+                        { loader: 'sass-loader', options: { sourceMap: true } },
+                    ],
+                }),
             },
             {
                 test: /\.js$/,
                 include: path.resolve(__dirname, 'src'),
                 exclude: /(node_modules)/,
-                loader: 'babel-loader'
+                loader: 'babel-loader',
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?[\s\S]+)?$/,
                 use: 'file-loader?name=bgimages/[name].[ext]',
-            }
-        ]
+            },
+        ],
     },
     plugins: [
-      new ExtractTextPlugin('style.css'),
-      // Name of the css bundle compiled in the './dist/ folder
-      new BrowserSyncPlugin({
-        host: 'localhost',
-        port: 3000,
-        proxy: 'http://localhost:8080/'
+        new ExtractTextPlugin('style.css'),
+        // Name of the css bundle compiled in the './dist/ folder
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            proxy: 'http://localhost:8080/',
         }),
-      new webpack.ProvidePlugin({
+        new webpack.ProvidePlugin({
             jQuery: 'jquery',
-            $: 'jquery'
-        })
-    ]
+            $: 'jquery',
+        }),
+    ],
 };
